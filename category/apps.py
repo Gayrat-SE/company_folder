792665242aaa76c5d21f8030c425c9e1302419a8
Gayrat-Dev <97515743+Gayrat-Dev@users.noreply.github.com>
2022-03-08 12:05:32 +0500

    third

--- category/apps.py
+++ category/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class CompanyAppConfig(AppConfig):
+class CategoryConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'company_app'
+    name = 'category'
